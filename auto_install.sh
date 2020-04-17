2c82c6f0e3057321d9cc69b204801544f891a0bf
mike <mgoebel@ucsb.edu>
2020-04-17 11:59:15 -0700

    first commit

---
+++ auto_install.sh
@@ -0,0 +1,94 @@
+packages="
+python=2.7.15~rc1-1
+python-dev=2.7.15~rc1-1
+python-virtualenv=15.1.0+ds-1.1
+python-pip=9.0.1-2.3~ubuntu1.18.04.1
+python-paver=1.2.1-1.1
+libxml2-dev=2.9.4+dfsg1-6.1ubuntu1.3
+libxslt1-dev=1.1.29-5ubuntu0.2
+libhdf5-dev=1.10.0-patch1+docs-4
+mercurial=4.5.3-1ubuntu2.1
+git=1:2.17.1-1ubuntu0.6
+cmake=3.10.2-1ubuntu2.18.04.1
+pkg-config=0.29.1-0ubuntu2
+graphviz=2.40.1-2
+libgraphviz-dev=2.40.1-2
+postgresql=10+190ubuntu0.1
+postgresql-client=10+190ubuntu0.1
+libsqlite3-dev=3.22.0-1ubuntu0.3
+libmysqlclient-dev=5.7.29-0ubuntu0.18.04.1
+openslide-tools=3.4.1+dfsg-2
+python-openslide=1.1.1-2ubuntu4
+libopenslide-dev=3.4.1+dfsg-2
+libopenslide0=3.4.1+dfsg-2
+libpq-dev=10.12-0ubuntu0.18.04.1
+libfftw3-dev=3.3.7-1
+libbz2-dev=1.0.6-8.1ubuntu0.2
+liblcms2-dev=2.9-1ubuntu0.1
+zlib1g-dev=1:1.2.11.dfsg-0ubuntu2
+libtiff-dev=4.0.9-5ubuntu0.3
+libpng-dev=1.6.34-1ubuntu0.18.04.2
+libjpeg62=1:6b2-3
+liborc-0.4-0=1:0.4.28-1
+liborc-0.4-dev=1:0.4.28-1
+qt5-default=5.9.5+dfsg-0ubuntu2.5
+qt5-qmake=5.9.5+dfsg-0ubuntu2.5
+libtheora0=1.1.1+dfsg.1-14
+libtheora-dev=1.1.1+dfsg.1-14
+liblzma5=5.2.2-1.3
+liblzma-dev=5.2.2-1.3
+libbz2-1.0=1.0.6-8.1ubuntu0.2
+libbz2-dev=1.0.6-8.1ubuntu0.2
+libxvidcore4=2:1.3.5-1
+libxvidcore-dev=2:1.3.5-1
+libgdcm2.8=2.8.4-1build2
+"
+
+# install system packages
+sudo apt-get install -y ${packages}
+
+# build openjpeg
+git clone https://github.com/uclouvain/openjpeg
+cd openjpeg && mkdir build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+sudo make -j4 install && sudo ldconfig && cd ../..
+
+# build liboil (required by schroedinger)
+wget https://liboil.freedesktop.org/download/liboil-0.3.13.tar.gz
+tar -xvzf liboil-0.3.13.tar.gz
+cd liboil-0.3.13/ && mkdir build && cd build/ && ../configure
+make && sudo make install && cd ../..
+
+# build schroedinger
+wget https://launchpad.net/schroedinger/trunk/1.0.0/+download/schroedinger-1.0.0.tar.gz
+tar -xvzf schroedinger-1.0.0.tar.gz && cd schroedinger-1.0.0/
+mkdir build && cd build/ && ../configure && make
+sudo make install && cd ../..
+
+# build imgcnv
+hg clone --insecure http://biodev.ece.ucsb.edu/hg/imgcnv && cd imgcnv
+#scp mike@128.111.185.28:~/fall_2019/build_bisque4/imgcnv/ubuntu1804.sh .
+cp $(dirname "$0")/ubuntu1804.sh .
+bash ubuntu1804.sh && make -j4 && sudo make install && cd ../
+sudo ln -s /usr/lib/libimgcnv.so.2 /usr/lib/libimgcnv.so && sudo ldconfig
+
+# create virtualenv
+sudo pip install virtualenvwrapper
+echo -e 'export PATH=/usr/local/bin:$PATH' >> ~/.bashrc
+echo -e 'export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python' >> ~/.bashrc
+echo -e 'export VIRTUALENVWRAPPER_VIRTUALENV=/usr/bin/virtualenv' >> ~/.bashrc
+echo -e 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
+source ~/.bashrc
+mkvirtualenv -p /usr/bin/python2 bqdev
+
+# clone bisque and pip install (there are problems with the biodev index, so reinstall some packages)
+git clone https://github.com/UCSB-VRL/bisque.git bisque-stable
+pip install -i https://biodev.ece.ucsb.edu/py/bisque/xenial/+simple/ -r requirements.txt
+pip install --force-reinstall lxml==3.7.3 orderedset==2.0.1 tables==3.4.2
+
+paver setup
+bq-admin setup
+
+
+
+
---
+++ ubuntu1804.sh
@@ -0,0 +1,133 @@
+# This file makes changes to several files to compile on Ubuntu 18.04.3 LTS.
+# All changes are done using replace and instert functions defined near the top.
+# The functions use vi in command line mode (ex).
+# Run the following commands:
+#
+#    bash ubuntu1804.sh
+#    make full -j4
+#    sudo make install
+#
+
+
+
+TAB=$'\t'
+NEWLINE=$'\n'
+replace () { ex -s -c "${line_num}d" -c "${line_num}i|${new_line}" -c x ${file}; }
+insert () { ex -s -c "${line_num}i|${new_line}${NEWLINE}" -c x ${file}; }
+
+
+if [ -f 'old_makefile' ]; then
+    cp 'old_makefile' 'Makefile'
+else
+    cp 'Makefile' 'old_makefile'
+fi
+
+
+# Put paranethesis around vaiables
+file='Makefile'
+old_line='PKG_CONFIG_PATH=$LIBVPX/vpx.pc:$LIBX264/x264.pc:$LIBX265/build/linux/x265.pc'
+new_line='PKG_CONFIG_PATH=$(PWD)/$(LIBVPX)/:$(PWD)/$(LIBX264)/:$(PWD)/$(LIBX265)/build/linux/'
+line_num=15
+replace
+
+# change .sh to .pl, .sh file does not exist but .pl does
+file='Makefile'
+old_line=${TAB}'(cd $(LIBVPX); chmod -f u+x build/make/rtcd.sh)'
+new_line=${TAB}'(cd $(LIBVPX); chmod -f u+x build/make/rtcd.pl)'
+line_num=65
+replace
+
+# change c++ standard to 98
+file='Makefile'
+old_line=${TAB}'(cd $(LIBX265)/build/linux; cmake -G "Unix Makefiles" ../../source -DCMAKE_CXX_FLAGS=-fPIC -DCMAKE_C_FLAGS=-fPIC )'
+new_line=${TAB}'(cd $(LIBX265)/build/linux; cmake -G "Unix Makefiles" ../../source -DCMAKE_CXX_FLAGS="-fPIC -std=c++98" -DCMAKE_C_FLAGS=-fPIC )'
+line_num=88
+replace
+
+# copy all x265 library files (.pc, etc) not just .a
+file='Makefile'
+old_line=${TAB}'(cp $(LIBX265)/build/linux/libx265.a $(LIBS)/)'
+new_line=${TAB}'(cp $(LIBX265)/build/linux/*265* $(LIBS)/)'
+line_num=90
+replace
+
+# added line to modify permissions on shell file
+file='Makefile'
+new_line=${TAB}'(cd $(LIBJPEGTURBO); chmod -f u+x simd/nasm_lt.sh)'
+line_num=98
+insert
+
+# change enable-pic to with-pic
+file='Makefile'
+old_line=${TAB}'(cd $(LIBJPEGTURBO); ./configure --enable-pic --enable-static --enable-shared=no )'
+new_line=${TAB}'(cd $(LIBJPEGTURBO); ./configure --with-pic --enable-static --enable-shared=no )'
+line_num=99
+replace
+
+# export pkg-config path
+file='Makefile'
+old_line=${TAB}'(cd $(FFMPEG)/ffmpeg-obj; ../ffmpeg/configure \'
+new_line=${TAB}'(export PKG_CONFIG_PATH=$(PKG_CONFIG_PATH); cd $(FFMPEG)/ffmpeg-obj; ../ffmpeg/configure \'
+line_num=117
+replace
+
+
+# stdbool.h not imported
+file='libsrc/libx265/source/x265.h'
+old_line=' '
+new_line='#include <stdbool.h>'
+line_num=26
+replace
+
+# export PKG_CONFIG_PATH
+file='Makefile'
+old_line=${TAB}'(cd $(FFMPEG)/ffmpeg-obj; ../ffmpeg/configure \'
+new_line=${TAB}'(cd $(FFMPEG)/ffmpeg-obj; export PKG_CONFIG_PATH=$(PKG_CONFIG_PATH); ../ffmpeg/configure \'
+line_num=117
+replace
+
+# The next four changes add the x265 build path to ffmpeg search path
+file='Makefile'
+old_line=${TAB}${TAB}'--extra-cflags="-fPIC -I../../libvpx -I../../libx264 -I../../libx265/source" \'
+new_line=${TAB}${TAB}'--extra-cflags="-fPIC -I../../libvpx -I../../libx264 -I../../libx265/source -I../../libx265/build/linux" \'
+line_num=120
+replace
+
+file='Makefile'
+old_line=${TAB}${TAB}'--extra-cxxflags="-fPIC -I../../libvpx -I../../libx264 -I../../libx265/source" \'
+new_line=${TAB}${TAB}'--extra-cxxflags="-fPIC -I../../libvpx -I../../libx264 -I../../libx265/source -I../../libx265/build/linux" \'
+line_num=121
+replace
+
+# Error between perl versions 2.25 and 2.26, the first brace needed to be escaped
+file='libsrc/ffmpeg/ffmpeg/doc/texi2pod.pl'
+old_line=${TAB}'s/\@anchor{(?:[^\}]*)\}//g;'
+new_line=${TAB}'s/\@anchor\{(?:[^\}]*)\}//g;'
+line_num=387
+replace
+
+# The next four lines add libraries to the end of the include paths on command line
+file='src/imgcnv.pro'
+old_line='    LIBS += -lpthread -lxvidcore -lopenjpeg -lschroedinger-1.0 -ltheora -ltheoraenc -ltheoradec'
+new_line='    LIBS += -lpthread -lxvidcore -lopenjp2 -lschroedinger-1.0 -ltheora -ltheoraenc -ltheoradec -llzma -lfftw3 -lbz2 -ldl'
+line_num=279
+replace
+
+file='src/imgcnv.pro'
+old_line='  }'
+new_line='    LIBS += -lpthread -lxvidcore -lopenjp2 -lschroedinger-1.0 -ltheora -ltheoraenc -ltheoradec -llzma -lfftw3 -lbz2 -ldl }'
+line_num=319
+replace
+
+file='src_dylib/libimgcnv.pro'
+old_line='    LIBS += -lpthread -lxvidcore -lopenjpeg -lschroedinger-1.0 -ltheora -ltheoraenc -ltheoradec'
+new_line='    LIBS += -lpthread -lxvidcore -lopenjp2 -lschroedinger-1.0 -ltheora -ltheoraenc -ltheoradec -llzma -lfftw3 -lbz2 -ldl'
+line_num=280
+replace
+
+file='src_dylib/libimgcnv.pro'
+old_line='  }'
+new_line='    LIBS += -lpthread -lxvidcore -lopenjp2 -lschroedinger-1.0 -ltheora -ltheoraenc -ltheoradec -llzma -lfftw3 -lbz2 -ldl }'
+line_num=320
+replace
+
